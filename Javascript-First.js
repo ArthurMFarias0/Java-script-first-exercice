b36dbd96f7f2f99c7ec46a9d1efd5e6f48e5728f
Arthur <arthuremeunome@gmail.com>
2022-09-08 16:54:16 -0300

    Primeiro Exercicio JavaScript

---
+++ Javascript-First.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+
+const userName = prompt('Typer your name: ');
+    console.log(`The typed name was: ${userName}`);
+
+const weight = prompt('Write your weight: ');
+    console.log(`The typed weight was: ${weight}`);
+
+const height = prompt('Type your height: ');
+    console.log(`The typed height was: ${height}`)
+
+const dateOfAdmission = prompt('typer you dateOfAdmission: ');
+    console.log(`the typed date Of Admission was: ${dateOfAdmission}`)
+
+const diabetes = prompt('You have diabetes?: ')
+    console.log(`the answer to whether she has diabetes was: ${diabetes}`)
+
